694803162aae29157416b08b8c083e51e25156be
BBlobster <39467264+BBlobster@users.noreply.github.com>
2018-06-24 17:20:34 +0200

    Initial commit

---
+++ Assets/ButtonLeftClicked.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class ButtonLeftClicked : MonoBehaviour {
+
+    Sprite player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("ButtonLeftClicked");
+
+    }
+}
