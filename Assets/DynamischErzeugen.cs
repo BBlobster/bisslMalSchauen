da26476c8ce4276e885b0982a7aa744e6b78b906
BBlobster <39467264+BBlobster@users.noreply.github.com>
2018-09-03 20:35:05 +0200

    test
    test

--- Assets/DynamischErzeugen.cs
+++ Assets/DynamischErzeugen.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class DynamischErzeugen : MonoBehaviour {
     public GameObject sprite;
     public Transform parent;
+    public GameObject block;
+    public Rigidbody2D rb;
+
+    private System.Random zufall = new System.Random();
+    private int x,y, thrust;
     // Use this for initialization
     void Start ()
     {
-        Instantiate(sprite, new Vector3(x: 0, y: 50, z: 0), Quaternion.identity);
-        sprite.transform.SetParent(parent, false);
+        for(y = 0; y < 100; y++)
+        {
+            x = zufall.Next(-2, +3);
+            block = Instantiate(sprite, new Vector3(x: x*20, y: 50*y, z: 0), Quaternion.identity);
+            block.transform.SetParent(parent, false);
+        }
+        
     }
 
-
+    
 
     // Update is called once per frame
     void Update () {
-		
-	}
+
+    }
 }
