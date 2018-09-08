e0af49aefb51ed458c93a2eb68bad02c33188cf8
BBlobster <39467264+BBlobster@users.noreply.github.com>
2018-09-08 21:26:39 +0200

    pc
    pc

--- Assets/ButtonLeftClicked.cs
+++ Assets/ButtonLeftClicked.cs
@@ -17,11 +17,16 @@ public class ButtonLeftClicked : MonoBehaviour {
 
     private void OnMouseDown()
     {
+        var pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0, 1);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+
+
+        player.transform.localPosition = player.transform.localPosition + new Vector3 (-20, 0, 0);
+        Debug.Log("ButtonLeftClicked"+Screen.width);
 
         
-
-        player.SetPositionAndRotation(player.position+Vector3.left,Quaternion.identity);
-        Debug.Log("ButtonLeftClicked");
 
     }
 }
--- Assets/ButtonRightClicked.cs
+++ Assets/ButtonRightClicked.cs
@@ -16,9 +16,12 @@ public class ButtonRightClicked : MonoBehaviour {
         var pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp(pos.x,0,1);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
-        player.SetPositionAndRotation(player.position + Vector3.right, Quaternion.identity);
 
-        Debug.Log("ButtonLRightClicked");
+
+
+        player.transform.localPosition = player.transform.localPosition + new Vector3(20, 0, 0);
+
+        Debug.Log("ButtonLRightClicked " +Screen.width);
 
     }
 
